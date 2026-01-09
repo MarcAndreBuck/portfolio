ad5ac43bb80e65fa0f3b72f201d661e3df280c09
Marc-Andre Buck <marcbuck94@gmail.com>
2026-01-09 14:05:16 +0100

    "feat(layout): add footer and adjust app layout"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LegalNoticeComponent } from './pages/legal-notice/legal-notice.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'legal-notice', component: LegalNoticeComponent }
 ];
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LanguageService } from '../i18n/language.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  lang = inject(LanguageService);
+  
 }
