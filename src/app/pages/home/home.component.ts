a624b858d03e285b64b509079ac1ac4383a336c9
Marc-Andre Buck <marcbuck94@gmail.com>
2026-01-12 20:03:56 +0100

    feat(home): add references, contact, projects, about and skills components

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './sections/hero/hero.component';
 import { SkillsComponent } from './sections/skills/skills.component';
+import { AboutComponent } from './sections/about/about.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
+import { ContactComponent } from './sections/contact/contact.component';
+import { ReferenceComponent } from './sections/reference/reference.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, SkillsComponent],
+  imports: [HeroComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactComponent, ReferenceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
