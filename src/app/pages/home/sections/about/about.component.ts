a3e1b46c3a889a7a8f7aaba73884f038f1ab877d
Marc-Andre Buck <marcbuck94@gmail.com>
2026-01-19 16:09:19 +0100

    feat(about): add initial About section layout and structure
    - implement About component with layout, card and media
    - add basic styling and structure
    - responsive behavior and copy are still work in progress

--- src/app/pages/home/sections/about/about.component.ts
+++ src/app/pages/home/sections/about/about.component.ts
@@ -1,9 +1,10 @@
+import { IconComponent } from '@/app/shared/icons/icon.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [IconComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
